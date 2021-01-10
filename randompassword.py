4b5b71c7b3ec6743fc39db19c2967f8d48347b8e
Astra <archdevil@pop-os.localdomain>
2021-01-10 15:38:09 +0530

    Making more systematic ....

--- randompassword.py
+++ randompassword.py
@@ -1,14 +1,14 @@
 def passward() :
     import random
-    n = int(input('Length of passward :'))
-    word = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
-    i = 0 
-    end = ""
+    n = int(input("Length of passward is : "))
+    word = "abcdefghijklmnopqrstuvWxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#$%"
+    i = 0
+    end = "" 
     while i < n :
        passw = random.choice(word)
-       end += passw
+       end+= passw
        i += 1
-    print(f'Your password is :{end}')   
+    print(f'Your password is : {end}')   
 
 if __name__ == "__main__":
     passward()
